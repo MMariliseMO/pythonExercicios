c68d3ab7863982d1663e5942948bcf2b2f04000a
Marilise Olimpio <marilisemarinho@gmail.com>
2025-02-28 13:56:34 -0300

    retirado as aspas triplas

--- ex004.py
+++ ex004.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 # com f-string
-'''algo = input('Digite algo: ')
+algo = input('Digite algo: ')
 print(f'\n0 tipo primitivo desse valor é: {type(algo)}')
 print(f'O valor contém apenas espaços? {algo.isspace()}')
 print(f'É um número? {algo.isnumeric()}')
@@ -10,10 +10,10 @@ print(f'É uma combinação de letras e números? {algo.isalnum()}')
 print(f'Todas as letras são maiúsculas? {algo.isupper()}')
 print(f'Todas as letras são minúsculas? {algo.islower()}')
 print(f'A 1º letra está maiúscula e o restante minúscula? {algo.istitle()}')
-'''
+
 
 # com o .format()
-'''a = input('Digite algo: ')
+a = input('Digite algo: ')
 print('\nO tipo primitivo desse valor é: ', type(a))
 print('O valor contém apenas espaços? ', a.isspace())
 print('É um número? ', a.isnumeric())
@@ -22,5 +22,5 @@ print('É uma combinação de letras e números? ', a.isalnum())
 print('Todas as letras são maiúsculas? ', a.isupper())
 print('Todas as letras são minúsculas? ', a.islower())
 print('A 1º letra está maiúsculas e o restante minúscula? ', a.istitle())
-'''
+
 
--- ex006.py
+++ ex006.py
@@ -1,25 +1,24 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 # usando o f-string e declarando apenas a variável num
-'''num = float(input('Digite um número: '))
+num = float(input('Digite um número: '))
 print(f'O número {num}, tem como o dobro {num * 2}, triplo {num * 3} e raiz quadrada {num ** 0.5:.2f}') # o ** 0.5:.2f, quer dizer: O ** 0.5 calcula a raiz quadrada do número, pois elevar o número a 1/2 é o mesmo que calcular a raiz quadrada e :.2f é que quero apenas 2 casas decimais.
-'''
+
 
 # função pow(base, expoente)
-'''num = float(input('Digite um número: '))
+num = float(input('Digite um número: '))
 print(f'O número {num}, tem como o dobro {num * 2}, como o triplo {num * 3} e raiz quadrada {pow(num, 1/2):.2f}') #{pow(num(base), 1/2(expoente)):.2f(2 casas decimais)}
-'''
+
 
 # declarando as variáveis e usando o .format()
-'''n = float(input('Digite um número: '))
+n = float(input('Digite um número: '))
 dobro = n * 2 
 triplo = n * 3
 raiz = n ** 0.5
 
 print('O número {}, tem como o dobro {}, triplo {} e raiz quadrada {:.2f}' .format(n, dobro, triplo, raiz))
-'''
+
 
 # usando apenas o .format()
-'''num = float(input('Digite um número: '))
+num = float(input('Digite um número: '))
 print('O numero {}, possui como dobro {}, triplo {} e raiz quadrada {:.2f}' .format(num, num * 2, num * 3, num ** 0.5))
-'''
