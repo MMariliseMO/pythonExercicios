4a6b9c0515e50879e9728a05c27c146a6a7bdefb
Marilise Olimpio <marilisemarinho@gmail.com>
2025-02-28 13:42:28 -0300

    correções simples

--- ex001.py
+++ ex001.py
@@ -1,13 +1,11 @@
 #Crie um programa que escreva "Olá, Mundo!" na tela.
 
-'''print('Olá, Mundo!')'''
+print('Olá, Mundo!')
 
-'''
 mensagem = 'Olá, Mundo!'
 print(mensagem)
-'''
 
 # aqui eu vou solicitar que o usuário digite a mensagem e seja impresso na tela a mensagem digitada pelo usuário.
-'''mensagem = input('Digite uma mensagem qualquer: ')
-print(f'A mensagem digitada é: {mensagem}.')'''
+mensagem = input('Digite uma mensagem qualquer: ')
+print(f'A mensagem digitada é: {mensagem}.')
 
