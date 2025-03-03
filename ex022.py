e357fed379db6339613afeff0b4bf79aa4fa1d12
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-03 08:57:06 -0300

    mais questões do curso de Python3 do Curso em Vídeo

---
+++ ex022.py
@@ -0,0 +1,1 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre: *O nome com todas as letras maiúsculas. *O nome com todas as letras minúsculas. *Quantas letras ao todo(sem contar os espaços). *Quantas letras tem o seu primeiro nome. 
