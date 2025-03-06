cf542da9eed408795cb37c02efb65fb0bebd49c8
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-06 10:11:17 -0300

    resolução de mais exercicios

--- ex030.py
+++ ex030.py
@@ -1,7 +1,7 @@
 #Crie um programa que leia um número inteiro e mostre na tela se ele é PAR o ÍMPAR.
 
 num = int(input('Digite um número qualquer: '))
-par = num % 2
+par = num % 2 #o número par tem resto 0 e número impar tem resto 1
 if par == 0:
     print(f'O número {num} é par.')
 else:
--- ex032.py
+++ ex032.py
@@ -1,1 +1,11 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+
+from datetime import date #para a data atual
+ano = int(input('Qual o ano que deseja saber se é ou não Bissexto? Digite o 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year #pega a data de hoje, porém só o ano
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é BISSEXTO!')
+else:
+    print('O ano não é BISSEXTO.')
