e0dce70779d4d77976603a5c0252f923d2c75809
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-06 09:34:34 -0300

    resolução do ex029.py ex030.py e ex031.py

--- ex029.py
+++ ex029.py
@@ -1,1 +1,9 @@
-#Escreva um programa que leia a veloidade de um carro. Se ele ultrapassar 80km/h, mostre um mensagem dizendo que ele será multado. A multa vai custar R$7,00 por cada Km acima do limite.
+#Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre um mensagem dizendo que ele será multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velocidade = float(input('Digite a velocidade atual do carro: '))
+multa = velocidade - 80
+if velocidade > 80:
+    print(f'Você será multado no valor de: R${multa * 7:.1f}, pois está acima do limite.')
+else:
+    print('Boa viagem! \nDirija com cuidado!')
+
--- ex030.py
+++ ex030.py
@@ -1,1 +1,15 @@
-#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR o ÍMPAR
+#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR o ÍMPAR.
+
+num = int(input('Digite um número qualquer: '))
+par = num % 2
+if par == 0:
+    print(f'O número {num} é par.')
+else:
+    print(f'O número {num} é impar.')
+
+
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+    print(f'O número {num} é par.')
+else: 
+    print(f'O número {num} é impar. ')
