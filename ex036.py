f49c670f2e7ff502e247b87f07a87a7fa2f4fa1d
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-07 17:19:23 -0300

    resolução dos ex036.py e ex037.py

--- ex036.py
+++ ex036.py
@@ -1,1 +1,11 @@
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador em quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+casa = float(input('Qual o valor da casa? R$ '))
+salario = float(input('Qual o valor do seu salário? R$ '))
+anos = int(input('Em quantos anos deseja pagar a casa? '))
+prestacao = casa / (anos * 12) #preciso converter os anos em meses
+if prestacao > 0.30 * salario:
+    print(f'Para pagar uma casa no valor de {casa:.2f} em {anos} anos a prestação é de R$ {prestacao:.2f}. \nEmpréstimo Negado!')
+else:
+    print(f'Para pagar uma casa no valor de {casa:.2f} em {anos} anos a prestação é de R$ {prestacao:.2f}. \nEmpréstimo Concedido!')
+
--- ex037.py
+++ ex037.py
@@ -1,1 +1,16 @@
 #Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal, 3 para hexadecimal.
+
+num = int(input('Digite um número inteiro qualquer: '))
+base = int(input('Qual a base que deseja usar para conversão? \n1 para binário; \n2 para octal; \n3 para hexadecimal. '))
+
+if base == 1:
+    print(f'O número {num} na base binária é igual a: {bin(num)}.')
+elif base == 2:
+    print(f'O número {num} na base octal é igual a: {oct(num)}.')
+elif base == 3:
+    print(f'O número {num} na base hexadecimal é igual a: {hex(num)}')
+else:
+    print('Opção inválida')
+
+
+    #https://docs.python.org/pt-br/3.10/library/functions.html#oct
