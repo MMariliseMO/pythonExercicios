9c5553895427fcc04c1a1d8b3b805d5cab55be54
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-12 11:50:53 -0300

    resolução dos exercícios 50 e 51

--- ex051.py
+++ ex051.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+primeiro_termo = int(input('Informe o 1º termo: '))
+razao = int(input('Informe a razão: '))
+decimo = primeiro_termo + (10 -1) * razao # a fórmula para encontrar o n-ésimo termo da PA é an = a1 + (n - 1) * r,
+
+for i in range(primeiro_termo, decimo + razao,  razao):
+    print(f'{i}', end = " ")
