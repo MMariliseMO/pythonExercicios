22bbdad9c504f6b0e400fc0042178d687f963e17
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-12 11:52:24 -0300

    erro no comentário

--- ex050.py
+++ ex050.py
@@ -1,7 +1,7 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar desconsidere-o.
 
 soma = 0 #acumulador
-cont = 0 #acumulador
+cont = 0 #contador
 for i in range(1, 7):
     num = int(input(f'Digite o {i}º número: '))
     if num % 2 == 0:
