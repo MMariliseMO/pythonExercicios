292d75ec3b4d5b5adb569ae515db4e6bc5303b52
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-17 16:36:19 -0300

    exercícios 58 e 59 com a resolução também de guanabara

--- ex058.py
+++ ex058.py
@@ -17,9 +17,22 @@ print(f'Parabéns você conseguiu acertar com {palpites} palpites. O número esc
 
 #Guanabara
 
-'''from random import randint
+from random import randint
 computador = randint(0, 10)
 print('Sou seu computador... Acabei de pensar em um número entre 0 e 10. ')
-print('Será que você consegue adivinhar qual foi? ')'''
+print('Será que você consegue adivinhar qual foi? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual o seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais! ... Tente mais uma vez.')
+        else:
+            print('Menos!... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!' .format(palpites))
         
 
--- ex059.py
+++ ex059.py
@@ -6,6 +6,7 @@
 # [ 5 ] sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso.
 
+
 opcao = 0 #inicia com 0 para garantir que seja executada pelo menos uma vez
 num1 = float(input('Informe um número qualquer: '))
 num2 = float(input('Informe outro número qualquer: '))
@@ -19,25 +20,64 @@ while opcao != 5:
 [ 5 ] sair do programa: \n'''))
     if opcao == 1:
         soma = num1 + num2
-        print(f'A soma dos números {num1} + {num2} é = {soma}.\n')
+        print(f'\033[7;30mA soma dos números {num1} + {num2} é = {soma}.\033[m\n')
     elif opcao == 2:
         multiplicar = num1 * num2
-        print(f'A multiplicação dos números {num1} * {num2} é = {multiplicar}.\n')
+        print(f'\033[7;30mA multiplicação dos números {num1} * {num2} é = {multiplicar}.\033[m\n')
     elif opcao == 3:
         if num1 > num2:
-            print(f'O número {num1} é maior que {num2}.\n')
+            print(f'\033[7;30mO número {num1} é maior que {num2}.\033[m\n')
         elif num2 > num1:
-            print(f'O número {num2} é maior que {num1}.\n')
+            print(f'\033[7;30mO número {num2} é maior que {num1}.\033[m\n')
         else:
-            print('Números iguais! \n')
+            print('\033[7;30mNúmeros iguais!\033[m\n')
     elif opcao == 4:
         print('Escolha novos números! ')
-        num1 = float(input('Informe um número qualquer: '))
-        num2 = float(input('Informe um outro número qualquer: \n'))
+        num1 = int(input('Informe um número qualquer: '))
+        num2 = int(input('Informe um outro número qualquer: \n'))
     elif opcao == 5:
         print(' \033[7;30mHasta la vista, baby!\033[m')
     else:
         print('Opção inválida! Volte e escolha uma opção válida!')
     
+
+
+n1 = int(input('Primeiro Valor: '))
+n2 = int(input('Segundo Valor: '))
+opcao = 0
+while opcao != 5: 
+    print('''          
+    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa: ''')
+    opcao = int(input('Qual é a sua opção? '))
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}' .format(n1, n2, soma))
+    elif opcao == 2:
+        produto = n1 * n2
+        print('O resultado entre {} * {} é {}' .format(n1, n2, produto))
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+            print('Entre {} e {} o maior valor é {}' .format(n1, n2, maior))
+    elif opcao == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro Valor: '))
+        n2 = int(input('Segundo Valor: '))
+    elif opcao == 5:
+        print('Finalizando ...')
+    else:
+        print('Opção Inválida. Tente Novamente!')
+    print('=-=' * 10)
+print('Fim do programa! Volte Sempre!')
+
+
+
+
 
 
