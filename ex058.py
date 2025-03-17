ccae7559957b27c807d380b2c5789faaf27ddbf5
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-17 11:52:12 -0300

    resolução dos exercícios 58 e 59 apenas com o meu código, falta assistir as aulas e copiar o código que Guanabara fez. No index.py coloquei apenas uma demonstração da aula.

--- ex058.py
+++ ex058.py
@@ -1,3 +1,25 @@
 # Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
 #Exercício 28: Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+from random import randint #importei a função randint que gera números aleatórios
+palpites = 1 #a contagem dos palpites
+computador = randint(0, 3) # a variável computador para armazenar o número que o randint gerou
+
+jogador = int(input('Informe um número inteiro qualquer entre 0 e 10: ')) # peço ao usuário o palpite dele
+
+while jogador != computador: # inicio o while com a condição do palpite do jogador ser diferente do palpite do computador
+    jogador = int(input('Você errou! Tente mais uma vez. ')) # quando o paplpite for diferente a mensagem de erro aparece e solicita uma nova entrada 
+    palpites += 1 #contagem dos palpites
+
+print(f'Parabéns você conseguiu acertar com {palpites} palpites. O número escolhido pelo computador foi: {computador} e o número escolhido pelo jogador foi: {jogador}') #quando o jogador acerta o mesmo número do computador
+
+
+#Guanabara
+
+'''from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10. ')
+print('Será que você consegue adivinhar qual foi? ')'''
+        
+
--- ex059.py
+++ ex059.py
@@ -5,3 +5,39 @@
 # [ 4 ] novos números
 # [ 5 ] sair do programa
 # Seu programa deverá realizar a operação solicitada em cada caso.
+
+opcao = 0 #inicia com 0 para garantir que seja executada pelo menos uma vez
+num1 = float(input('Informe um número qualquer: '))
+num2 = float(input('Informe outro número qualquer: '))
+
+while opcao != 5:
+    opcao = int(input('''Escolha uma das opções abaixo:
+[ 1 ] somar
+[ 2 ] multiplicar
+[ 3 ] maior
+[ 4 ] novos números
+[ 5 ] sair do programa: \n'''))
+    if opcao == 1:
+        soma = num1 + num2
+        print(f'A soma dos números {num1} + {num2} é = {soma}.\n')
+    elif opcao == 2:
+        multiplicar = num1 * num2
+        print(f'A multiplicação dos números {num1} * {num2} é = {multiplicar}.\n')
+    elif opcao == 3:
+        if num1 > num2:
+            print(f'O número {num1} é maior que {num2}.\n')
+        elif num2 > num1:
+            print(f'O número {num2} é maior que {num1}.\n')
+        else:
+            print('Números iguais! \n')
+    elif opcao == 4:
+        print('Escolha novos números! ')
+        num1 = float(input('Informe um número qualquer: '))
+        num2 = float(input('Informe um outro número qualquer: \n'))
+    elif opcao == 5:
+        print(' \033[7;30mHasta la vista, baby!\033[m')
+    else:
+        print('Opção inválida! Volte e escolha uma opção válida!')
+    
+
+
