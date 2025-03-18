6f24a40efe0d30c89b3b0a19914d9446df96e2f8
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-18 10:28:33 -0300

    minha resolução do exercício ex060.py

--- ex060.py
+++ ex060.py
@@ -1,1 +1,21 @@
 # Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+from math import factorial
+import time
+num = int(input('Informe um número qualquer: '))
+fatorial = factorial(num)
+print(f'Calculando ...')
+time.sleep(1)
+print(f'{num}! = {fatorial}')
+
+
+
+    
+
+
+
+
+
+
+
+
