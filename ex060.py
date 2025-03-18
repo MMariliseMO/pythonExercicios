bef2570c1442ed89442b88a43d9928a5f09f4616
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-18 10:40:13 -0300

    resolução do Guanabara

--- ex060.py
+++ ex060.py
@@ -8,6 +8,18 @@ print(f'Calculando ...')
 time.sleep(1)
 print(f'{num}! = {fatorial}')
 
+#Guanabara
+n = int(input('Informe um número: '))
+c = n # c é a variável contador
+f = 1
+print('Calculando {}! = ' .format(n), end = '')
+while c > 0: #enquanto o contador for maior que 0
+    print('{}' .format(c), end = '')
+    print(' x ' if c > 1 else ' = ', end = '')
+    f *= c
+    c -= 1
+print('{}' .format(f))
+
 
 
     
