0e44a52e2ff44fad2c4e6ba182f805503047933b
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-19 10:31:06 -0300

    resolução do exercício ex061.py

--- ex061.py
+++ ex061.py
@@ -1,3 +1,12 @@
 # Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
 
 #Exercício 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+primeiro_termo = int(input('Informe o primeiro termo: '))
+razao = int(input('Informe a razão: '))
+termo = primeiro_termo #mostra o termo
+i = 1 #contar os termos
+while i <= 10:
+    print(f'{termo}', end = ' ')
+    termo += razao
+    i += 1
