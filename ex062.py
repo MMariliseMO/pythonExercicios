09fd6c87c04768702a53b89a36eff133f7a6e82e
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-20 11:13:34 -0300

    resolução dos exercícios 62 e 63

--- ex062.py
+++ ex062.py
@@ -1,1 +1,18 @@
 # Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
+
+primeiro = int(input('Informe o primeiro termo: '))
+razao = int(input('Informe a razão: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} - '.format(termo), end = '')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão Finalizada com {} termos mostrados' .format(total))
+print('FIM.')
