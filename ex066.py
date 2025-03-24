bc4fdfce82fcfe99759b216374abbff9f3d254e5
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-24 11:16:38 -0300

    correção simples

--- ex066.py
+++ ex066.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 
-n = cont = soma = 0
+num = cont = soma = 0
 while True:
     num = int(input('Informe um número qualquer: '))
     if num == 999:
