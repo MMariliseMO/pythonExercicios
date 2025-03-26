2530020f996e1c72f0f2ae416219699e2c823e41
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-26 10:57:16 -0300

    atualização

--- ex067.py
+++ ex067.py
@@ -1,12 +1,12 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 
-while True:
+while True: #loop infinito
     num = int(input('Informe um número: '))
     if num <= 0: # se for menor ou igual a zero
         break # sai do loop, pois é o número menor que 0
-    print('-' * 30)
+    print('*' * 30)
     print(f'-> TABUADA DO NÚMERO {num} <-')
     for i in range(1, 11):
         print(f'{num} x {i} = {num * i}')
-    print('-' * 30)
+    print('*' * 30)
 print('Fim do programa!')
