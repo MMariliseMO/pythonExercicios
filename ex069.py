af1e83709121e53f99cf21835f0bd4125deeaabb
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-26 21:14:32 -0300

    finalização do mundo 2 do curso de python3. Resolvido os ex068 ao ex0.70.

--- ex069.py
+++ ex069.py
@@ -2,3 +2,51 @@
 # A) quantas pessoas tem mais de 18 anos.
 # B) quantos homens foram cadastrados.
 # C) quantas mulheres tem menos de 20 anos.
+
+contador_idade = contador_homens = contador_mulheres = 0
+
+while True:
+    sexo = str(input('Informe o Sexo[M/F]: ')) .upper().strip()
+    idade = int(input('Informe a idade: '))
+
+    if idade > 18:
+        contador_idade += 1
+    if sexo == 'M':
+        contador_homens += 1
+    if sexo == 'F' and idade < 20:
+        contador_mulheres += 1
+
+    cadastro = str(input('Deseja continuar cadastrando? [S/N] ')) .upper().strip()
+    if cadastro == 'N':
+        break
+
+print('Cadastro realizado com sucesso!')
+print(f'Foram cadastrados {contador_idade} pessoas maiores de 18 anos.')
+print(f'{contador_homens} homens cadastrados.')
+print(f'E {contador_mulheres} mulheres com menos de 20 anos.')
+
+###################################
+
+tot18 = totH = totalM20 = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).upper().strip()[0]
+
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totalM20 += 1
+    
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {tot18}.')
+print(f'Ao todo temos {tot18} homens cadastrados.')
+print(f'Temos total {totalM20} mulheres com menos de 20 anos.')
+
--- ex070.py
+++ ex070.py
@@ -2,3 +2,56 @@
 # A) qual é o total gasto na compra.
 # B) quantos produtos custam mais de R$1000.
 # C) qual é o nome do produto mais barato.
+
+'''total_compra = total_produto = produto_mil = 0
+nome_produto_barato = ''
+preco_produto_barato = float('inf')
+
+while True:
+    nome_produto = str(input('Informe o nome do produto: ')).strip()
+    preco_produto = float(input('Informe o preço do produto: '))
+    
+    total_produto += preco_produto
+    
+    if preco_produto > 1000:
+        produto_mil += 1
+    
+    if preco_produto < preco_produto_barato:
+        preco_produto_barato = preco_produto
+        nome_produto_barato = nome_produto 
+    
+
+    cadastro = str(input('Deseja continuar? [S/N] ')).upper().strip()
+    if cadastro == 'N':
+        break
+
+print(f'O total gasto na compra foi de: {total_produto}.')
+print(f'{produto_mil} produtos custam mais de R$1000.00. ')
+print(f'{nome_produto_barato} foi o produto mais barato.')'''
+
+###########################################
+total = totmil = menor = cont = 0
+barato = ' '
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$'))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+            break
+
+print('{:-^40}' .format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
+
+
