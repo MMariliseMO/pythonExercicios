a7e73d60994ca7e413f301044962e525473b8c5e
Marilise Olimpio <marilisemarinho@gmail.com>
2025-03-27 10:12:30 -0300

    docs: Adiciona comentário explicando o for e a função range, além de explicar o uso do parametro end = ' '.

--- ex047.py
+++ ex047.py
@@ -4,5 +4,5 @@ for i in range(1, 51):
     if i % 2 == 0: #o número i será dividido por 2 com resto 0
         print(i, end = ' ')
 
-for i in range(2, 51, 2):
-    print(i, end = ' ')
+for i in range(2, 51, 2): #começa no índice 2 e pula de dois em dois números
+    print(i, end = ' ') #O uso do parametro "end = ' '" no print faz com que os valores sejam impressos na mesma linha, e separados por espaço.
